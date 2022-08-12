c1da685654de26a9f367227eb822000c9eff164a
Daniil Ermolovich <ermolovichd007@gmail.com>
2022-08-13 00:24:13 +0300

    init: start task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,25 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.left = 0;
+        this.right = 0;
+        this.mid = 0;
+    }
 
     setRange(min, max) {
-
+    return this.right = max, this.left = min;
     }
 
     guess() {
-
+        this.mid = Math.round((this.right - this.left)/2 + this.left)
+        return this.mid
     }
 
     lower() {
-
+        return this.right = this.mid; 
     }
 
     greater() {
-
+        return this.left = this.mid;
     }
 }
 
